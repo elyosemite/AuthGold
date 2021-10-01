42e43b5041f307c902a8fad1e735bf96f5acd9f6
Yuri Melo <yurifullstack@gmail.com>
2021-09-30 23:03:35 -0300

    New Controller has added for Logging

---
+++ Controllers/LogController.cs
@@ -0,0 +1,35 @@
+﻿using AuthGold.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AuthGold.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class LogController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Logging> Get()
+        {
+            var client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false");
+            var database = client.GetDatabase("LoggingEinblick");
+            var collection = database.GetCollection<Logging>("logging");
+
+
+            // Add
+
+            collection.InsertOne(new Logging
+            {
+                Identifier = ObjectId.GenerateNewId(),
+                Name = "Yuri Melo",
+                Time = DateTime.Now
+            });
+
+            return collection.Find(o => o.Name != null).ToList();
+        }
+    }
+}
