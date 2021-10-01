c07a476907be7450ce2a6f1964ff31ac27237c01
Yuri Melo <yurifullstack@gmail.com>
2021-09-30 23:03:38 -0300

    Logging entity

---
+++ Models/Logging.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace AuthGold.Models
+{
+    public class Logging
+    {
+        [BsonId]
+        public ObjectId Identifier { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("time")]
+        public DateTime Time { get; set; }
+    }
+}
